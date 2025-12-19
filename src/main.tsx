f2ff2a9cffe4363ae7e9b9583be8aa65d19ea1a1
Erick SHU <1037289945@qq.com>
2025-12-20 04:11:20 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 
+/*获取页面节点,并转换为虚拟DOM*/
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    /*将组件渲染到目标结点的页面上: 使用React严格语法，比如react过时的api*/
+    <StrictMode>
+        <App/>
+    </StrictMode>,
 )
